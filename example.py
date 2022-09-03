aa5676c1be856aaa967de8770548ac9cc8dadb01
Li Muzhi <109349321+askformeal@users.noreply.github.com>
2022-09-03 12:54:27 +0800

    Update example.py

--- example.py
+++ example.py
@@ -4,8 +4,7 @@ from pygame_matting import Matter
 
 #since my screen wasn't big enough, I can't display the entire picture
 
-screen  = pygame.display.set_mode((1000,700))
-image = pygame.Surface((749,941))
+screen  = pygame.display.set_mode((1000,700)
 my_mat = Matter('example.bmp',(30,77),(749,941))
 image = my_mat.mat()
 screen.blit(image,(0,0))
